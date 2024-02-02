42453875c37f623a81cc40ed4cb2465aa777fbf1
phianova <98988580+phianova@users.noreply.github.com>
2024-02-02 13:49:43 +0000

    basic setup

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import { Josefin_Sans } from "next/font/google";
+import "./globals.css";
+import Navigation from "@/components/Navigation";
+import Footer from "@/components/Footer";
+
+const josefin = Josefin_Sans({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Sophia Warren",
+  description: "Developer portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+
+      <body className={josefin.className}>{children}</body>
+    </html>
+  );
+}
