7aa3f51afedcd9ae599936f6beaa24f520d24d78
phianova <98988580+phianova@users.noreply.github.com>
2024-03-15 16:50:03 +0000

    add images and files

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-
+      <Navigation></Navigation>
       <body className={josefin.className}>{children}</body>
+      <Footer></Footer>
     </html>
   );
 }
